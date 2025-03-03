40df9d7714c1736eb6e50c0298b0a4febf5e42d1
simsononroad <163458590+simsononroad@users.noreply.github.com>
2025-03-03 18:59:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from flask import Flask, redirect, render_template, url_for, request, blueprints
 import sqlite3
-import psycopg2
 from templates.admin.admin import admin_pg
 
-try:
-    from key import db_name, user_name, pass_word, host
-except:
-    print('Csinálj a "key.py"  file-ba "db_name", "user_name","pass_word", "host", "port" változót a megfelelő értékekkel')
-    exit()
 
 
 SECRET_KEY = 'development'
@@ -15,15 +9,6 @@ SECRET_KEY = 'development'
 app = Flask(__name__)
 app.register_blueprint(admin_pg, url_prefix="/admin")
 
-
-def init_db():
-    conn = psycopg2.connect(
-                    database=db_name,
-                    host=host,
-                    user=user_name,
-                    password=pass_word,
-                    port=5433)
-    return conn
 
 
 @app.route("/")
@@ -36,17 +21,7 @@ def szentiras():
 
 @app.route("/teremtestortenet")
 def teremtes_tortenet():
-    con = init_db()
-    cur = con.cursor()
-    
-    cur.execute("SELECT teremtes FROM public.szovegek;")
-    teremtes_tortenet_szoveg = cur.fetchall()[0][0]
-    teremtes_tortenet_sorok = str(teremtes_tortenet_szoveg).split("\n")
-
-    
-    
-    
-    return render_template("teremtes.html", teremtes_tortenet_sorok=teremtes_tortenet_sorok)
+    return render_template("teremtes.html")
 
 @app.route("/zarandokhely")
 def zarandokhely():
