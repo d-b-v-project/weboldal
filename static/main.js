b6779c86e647e9df684c6b029d5ba10ad0406bc5
simsononroad <d-github@kopaszhegy.hu>
2025-03-18 22:35:29 +0100

    reszponzivitás megcsinálva

--- static/main.js
+++ static/main.js
@@ -1,8 +1,21 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  var y = document.getElementById("login");
+  if (x.className === "topnav") {
+    y.style.display = "none"
+    
+    x.className += " responsive";
+  } else {
+    y.style.display = "block"
+    x.className = "topnav";
+  }
+}
+
+function myFunctions() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+  x.className += " responsive";
+} else {
+  x.className = "topnav";
+}
 }
